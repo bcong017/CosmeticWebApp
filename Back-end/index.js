ebe35c4044d5bf27c24538a0d86093fdd17eec3e
Duy-0470 <20520470@gm.uit.edu.vn>
2023-11-15 16:34:52 +0700

    minor changes

--- Back-end/index.js
+++ Back-end/index.js
@@ -6,7 +6,7 @@ const app = express();
 const db = mysql.createConnection({
     host: 'localhost',
     user: 'DA2',
-    password: 'hehehelol123',
+    password: '480963b8ff099c6620ac6935d79aa6242cb81bd74d5fd805577adaf4f2522472a6a716f23c69eb0e038cd0dc8eb16e36',
     database: 'cosmetic'
 });
 
