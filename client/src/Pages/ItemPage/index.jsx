1cf8479452dbcb1e6849955152be2441c07715d7
bcong017 <20520417@gm.uit.edu.vn>
2023-11-29 21:51:30 +0700

    Tạm xong item page

--- client/src/Pages/ItemPage/index.jsx
+++ client/src/Pages/ItemPage/index.jsx
@@ -1,8 +1,28 @@
-import { useRef } from "react";
+import { useEffect, useRef, useState } from "react";
 import "./style.css";
 
 function ItemPage({ amount = 4 }) {
+  const itemDetailTabsRef = useRef();
   const imgRef = useRef();
+  const [activeIndex,setActiveIndex] = useState(0);
+
+  useEffect(() => {
+    for (let i = 0; i < 5; i++)
+    {
+      if (i!=activeIndex)
+      {
+        itemDetailTabsRef.current.childNodes[i].className = 'item-detail-tab'
+      }
+      else
+      {
+        itemDetailTabsRef.current.childNodes[i].className = 'item-detail-tab active-color'
+      }
+    }
+  }
+
+  ,[activeIndex])
+
+  
   return (
     <div className="item-page-body">
       <div className="item-media-side-detail-block">
@@ -91,9 +111,33 @@ function ItemPage({ amount = 4 }) {
             )}
           </div>
           <button className="add-item-btn">
-            Thêm vào giỏ hàng
+            Thêm vào giỏ hàng &nbsp;
             <i className="fa-solid fa-cart-shopping cart-icon-in-button"></i>
           </button>
+        </div>
+      </div>
+      <div className="item-detail">
+        <div className="item-detail-tabs" ref={itemDetailTabsRef}>
+          <div className="item-detail-tab" data-key='0' onClick={(e) => setActiveIndex(e.target.getAttribute("data-key")) }>Thông tin sản phẩm</div>
+          <div className="item-detail-tab" data-key='1' onClick={(e) => setActiveIndex(e.target.getAttribute("data-key")) }>Thông số sản phẩm</div>
+          <div className="item-detail-tab" data-key='2' onClick={(e) => setActiveIndex(e.target.getAttribute("data-key")) }>Thành phần sản phẩm</div>
+          <div className="item-detail-tab" data-key='3' onClick={(e) => setActiveIndex(e.target.getAttribute("data-key")) }>Hướng dẫn sử dụng</div>
+          <div className="item-detail-tab" data-key='4' onClick={(e) => setActiveIndex(e.target.getAttribute("data-key")) }>Đánh giá</div>
+        </div>
+        <div className="selected-tab-detail-container" >
+          Sed ut perspiciatis unde omnis iste natus error sit voluptatem
+          accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae
+          ab illo inventore veritatis et quasi architecto beatae vitae dicta
+          sunt explicabo. Nemo enim ipsam voluptatem quia voluptas sit
+          aspernatur aut odit aut fugit, sed quia consequuntur magni dolores eos
+          qui ratione voluptatem sequi nesciunt. Neque porro quisquam est, qui
+          dolorem ipsum quia dolor sit amet, consectetur, adipisci velit, sed
+          quia non numquam eius modi tempora incidunt ut labore et dolore magnam
+          aliquam quaerat voluptatem. Ut enim ad minima veniam, quis nostrum
+          exercitationem ullam corporis suscipit laboriosam, nisi ut aliquid ex
+          ea commodi consequatur? Quis autem vel eum iure reprehenderit qui in
+          ea voluptate velit esse quam nihil molestiae consequatur, vel illum
+          qui dolorem eum fugiat quo voluptas nulla pariatur?
         </div>
       </div>
     </div>
