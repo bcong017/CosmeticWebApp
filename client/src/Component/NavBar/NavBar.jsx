8a98dcbac29cbafbef53864f7f1ff927ab621c09
bcong017 <20520417@gm.uit.edu.vn>
2023-12-02 15:21:08 +0700

    upd: navbar

--- client/src/Component/NavBar/NavBar.jsx
+++ client/src/Component/NavBar/NavBar.jsx
@@ -1,90 +1,134 @@
+import { useState } from 'react';
 import { Link } from 'react-router-dom';
 import SearchBar from '../SearchBar/SearchBar.jsx';
+import {
+  Button,
+  Dropdown,
+  DropdownMenu,
+  DropdownItem,
+  DropdownTrigger,
+  Navbar,
+  NavbarBrand,
+  NavbarContent,
+  NavbarItem,
+  Tooltip,
+} from '@nextui-org/react';
 import './NavBar.css';
 
+const category = [
+  {
+    title: 'Chăm sóc da mặt',
+    children: {
+      faceRemover: 'Tẩy trang mặt',
+      cleanser: 'Sữa rửa mặt',
+      exfoliating: 'Tẩy tế bào chết',
+      lipBalm: 'Dưỡng môi',
+    },
+  },
+  {
+    title: 'Trang điểm mặt',
+    children: {
+      creampie: 'Kem lót',
+      foundation: 'Kem nền',
+      cushion: 'Phấn nước Cushion',
+    },
+  },
+  {
+    title: 'Chăm sóc tóc và da đầu',
+    children: {
+      shampoo: 'Dầu gội',
+      conditioner: 'Dầu xả',
+      hairCare: 'Dưỡng tóc',
+    },
+  },
+  {
+    title: 'Nước hoa',
+    children: {
+      women: 'Nước hoa nữ',
+      men: 'Nước hoa nam',
+      fullBody: 'Xịt thơm toàn thân',
+      intimate: 'Nước hoa vùng kín',
+    },
+  },
+];
+
 function NavBar() {
+  const [isOpenCategory, setIsOpenCategory] = useState(false);
+
+  const toggleCategory = (value) => {
+    setIsOpenCategory(value);
+  };
+
   return (
     <>
-      <div id='header'>
-        {/*begin nav*/}
-        <Link id='Logo' to='/'>
-          <img src='../Full_Logo.png' alt='Logo' />
-        </Link>
-        <a className='category' href='#'>
-          Danh mục
-          <i className='fa-solid fa-arrow-down arrow-down-icon'></i>
-          <div className='container'>
-            <ul className='subnav'>
-              <span> Chăm sóc da mặt</span>
-              <li>
-                <a href='#'>Tẩy trang mặt</a>
-              </li>
-              <li>
-                <a href='#'>Sữa rửa mặt</a>
-              </li>
-              <li>
-                <a href='#'>Tẩy tế bào chết</a>
-              </li>
-              <li>
-                <a href='#'>Dưỡng môi</a>
-              </li>
-            </ul>
-            <ul className='subnav'>
-              <span> Trang điểm mặt</span>
-              <li>
-                <a href='#'>Kem lót</a>
-              </li>
-              <li>
-                <a href='#'>Kem nền</a>
-              </li>
-              <li>
-                <a href='#'>Phấn nước Cushion</a>
-              </li>
-            </ul>
-            <ul className='subnav'>
-              <span> Chăm sóc tóc và da đầu</span>
-              <li>
-                <a href='#'>Dầu gội</a>
-              </li>
-              <li>
-                <a href='#'>Dầu xả</a>
-              </li>
-              <li>
-                <a href='#'>Dưỡng tóc</a>
-              </li>
-            </ul>
-            <ul className='subnav'>
-              <span> Nước hoa</span>
-              <li>
-                <a href='#'>Nước hoa nữ</a>
-              </li>
-              <li>
-                <a href='#'>Nước hoa nam</a>
-              </li>
-              <li>
-                <a href='#'>Xịt thơm toàn thân</a>
-              </li>
-              <li>
-                <a href='#'>Nước hoa vùng kín</a>
-              </li>
-            </ul>
-          </div>
-        </a>
-        <SearchBar></SearchBar>
-        <div className='tooltip'>
-          <Link to='/'>
-            <i className='fa-solid fa-circle-user user-icon'></i>
+      <Navbar isBlurred={false} isBordered maxWidth='full'>
+        <NavbarBrand className='hidden sm:flex'>
+          <Link id='Logo' to='/'>
+            <img src='../Full_Logo.png' alt='Logo' />
           </Link>
-          <span className='tooltiptext'>Đăng nhập / Đăng ký</span>
-        </div>
-        <div className='tooltip'>
-          <Link to='/'>
-            <i className='fa-solid fa-cart-shopping cart-icon'></i>
-          </Link>
-          <span className='tooltiptext'>Giỏ hàng</span>
-        </div>
-        {/*end nav*/}
-      </div>
+        </NavbarBrand>
+
+        <NavbarContent className='flex gap-8 md:flex gap-4' justify='end'>
+          <NavbarItem>
+            <Dropdown
+              closeOnSelect={false}
+              showArrow
+              isOpen={isOpenCategory}
+              onOpenChange={setIsOpenCategory}
+              onMouseLeave={() => toggleCategory(false)}
+            >
+              <DropdownTrigger onMouseEnter={() => toggleCategory(true)}>
+                <Button variant='light' radius='none'>
+                  Danh mục
+                </Button>
+              </DropdownTrigger>
+
+              <DropdownMenu aria-label='category' disallowEmptySelection>
+                {category.map((el, index) => (
+                  <DropdownItem key={index}>
+                    <Dropdown
+                      type='listbox'
+                      size='sm'
+                      showArrow
+                      placement='right'
+                    >
+                      <DropdownTrigger>{el.title}</DropdownTrigger>
+
+                      <DropdownMenu disallowEmptySelection variant='light'>
+                        {Object.keys(el.children).map((key) => (
+                          <DropdownItem key={key}>
+                            <Link to='/test'>{el.children[key]}</Link>
+                          </DropdownItem>
+                        ))}
+                      </DropdownMenu>
+                    </Dropdown>
+                  </DropdownItem>
+                ))}
+              </DropdownMenu>
+            </Dropdown>
+          </NavbarItem>
+
+          <NavbarItem>
+            <SearchBar />
+          </NavbarItem>
+
+          <NavbarItem>
+            <Tooltip content='Đăng nhập / Đăng ký'>
+              <Link to='/'>
+                <div className='fa-solid fa-circle-user user-icon' />
+              </Link>
+            </Tooltip>
+          </NavbarItem>
+
+          <NavbarItem>
+            <Tooltip content='Giỏ hàng'>
+              <Link to='/'>
+                <div className='fa-solid fa-cart-shopping cart-icon' />
+              </Link>
+            </Tooltip>
+          </NavbarItem>
+        </NavbarContent>
+      </Navbar>
     </>
   );
 }
