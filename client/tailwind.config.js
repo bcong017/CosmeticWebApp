51dfb074cf1a43078179255b7f6c0e96463b8982
bcong017 <20520417@gm.uit.edu.vn>
2023-12-05 22:25:29 +0700

    upd: tailwind config

--- client/tailwind.config.js
+++ client/tailwind.config.js
@@ -1,20 +1,21 @@
-const { nextui } = require("@nextui-org/react");
+const { nextui } = require('@nextui-org/react');
 
 /** @type {import('tailwindcss').Config} */
 export default {
   content: [
-    "./index.html",
-    "./src/**/*.{js,ts,jsx,tsx}",
-    "./node_modules/@nextui-org/theme/dist/**/*.{js,ts,jsx,tsx}",
+    './index.html',
+    './src/**/*.{js,ts,jsx,tsx}',
+    './node_modules/@nextui-org/theme/dist/**/*.{js,ts,jsx,tsx}',
   ],
   theme: {
     extend: {
       colors: {
-        "heavy-pink": "#f875aa",
-        "section-pink": "#f3cdcdc2",
+        'heavy-pink': '#f875aa',
+        'section-pink': '#f3cdcdc2',
+        'primary-pink': '#ffdfdf',
       },
     },
   },
-  darkMode: "class",
+  darkMode: 'class',
   plugins: [nextui()],
 };
